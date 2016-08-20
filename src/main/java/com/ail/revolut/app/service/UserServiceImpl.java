8a79d55731eb66d56d367503f7647302d5237931
Arthur Lomsadze <ailman1985@gmail.com>
2016-08-21 00:24:12 +0300

    Added UserServiceTest and UserServiceImpl

---
+++ src/main/java/com/ail/revolut/app/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ail.revolut.app.service;
+
+import com.ail.revolut.app.model.User;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public User createUser(String userName) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public User getUser(long id) {
+		// TODO
+		return null;
+	}
+}
---
+++ src/test/java/com/ail/revolut/app/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.ail.revolut.app.service;
+
+import org.junit.Test;
+
+public class UserServiceTest {
+
+	private UserService userService = new UserServiceImpl();
+
+	@Test
+	public void testCreateUser() {
+		// TODO
+	}
+}
