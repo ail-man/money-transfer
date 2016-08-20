95c1fbcd3de23ed2d5653ba77e135428b7d168d1
Arthur Lomsadze <ailman1985@gmail.com>
2016-08-20 16:36:51 +0300

    Added hibernate example

---
+++ src/main/java/com/ail/revolut/app/model/User.java
@@ -0,0 +1,46 @@
+package com.ail.revolut.app.model;
+
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table(name = "users")
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "name", nullable = false, unique = true, length = 50)
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+				.append("id", id)
+				.append("name", name)
+				.toString();
+	}
+}
