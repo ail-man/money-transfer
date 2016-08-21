99696ad2759d440755f598a9cb2c80016e267f97
Arthur Lomsadze <ailman1985@gmail.com>
2016-08-21 21:00:09 +0300

    Develop main class

--- src/main/java/com/ail/revolut/MoneyTransferApp.java
+++ src/main/java/com/ail/revolut/MoneyTransferApp.java
@@ -6,36 +6,31 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.net.URI;
 
 import static com.example.jersey.Main.BASE_URI;
 
 public class MoneyTransferApp {
+	private static final String APP_PACKAGE = "com.ail.revolut";
 	private static final Logger logger = LoggerFactory.getLogger(MoneyTransferApp.class);
 
 	public static void main(String[] args) {
 		try {
-			initJersey();
-		} catch (Exception e) {
+			final HttpServer server = startServer();
+			logger.info(String.format(
+					"Jersey app started with WADL available at %sapplication.wadl Hit enter to stop it...",
+					BASE_URI)
+			);
+			System.in.read();
+			server.shutdown();
+		} catch (Throwable e) {
 			logger.error(e.getMessage(), e);
 		}
 	}
 
-	private static void initJersey() throws IOException {
-		final HttpServer server = startServer();
-		logger.info(String.format(
-				"Jersey app started with WADL available at %sapplication.wadl Hit enter to stop it...",
-				BASE_URI)
-		);
-		System.in.read();
-		server.shutdown();
-	}
-
-	private static HttpServer startServer() {
-		final ResourceConfig rc = new ResourceConfig().packages("ail.revolut");
+	public static HttpServer startServer() {
+		final ResourceConfig rc = new ResourceConfig().packages(APP_PACKAGE);
 		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
 	}
-
 
 }
