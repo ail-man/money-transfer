43a81cbad82333bcc71cd2747cca6ed82bb68dbf
Arthur Lomsadze <ailman1985@gmail.com>
2016-08-21 22:20:15 +0300

    Fix RemittanceService with tests

--- src/test/java/com/ail/revolut/app/service/RemittanceServiceTest.java
+++ src/test/java/com/ail/revolut/app/service/RemittanceServiceTest.java
@@ -1,4 +1,18 @@
 package com.ail.revolut.app.service;
 
+import com.ail.revolut.app.model.Remittance;
+import org.junit.Test;
+
 public class RemittanceServiceTest {
+
+	RemittanceService remittanceService = new RemittanceServiceImpl();
+
+	@Test
+	public void test() {
+		Remittance remittance = new Remittance();
+		remittance.setFrom(1L);
+		remittance.setTo(2L);
+		remittance.setAmount(300L);
+		remittanceService.save(remittance);
+	}
 }
