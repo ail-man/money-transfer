2ca601c74338a3e1f3564ecd3baf7ce69c76caab
Arthur Lomsadze <ailman1985@gmail.com>
2016-08-21 14:36:45 +0300

    Remove IAccountTest and TODOs

--- src/main/java/com/ail/revolut/app/service/AccountService.java
+++ src/main/java/com/ail/revolut/app/service/AccountService.java
@@ -3,7 +3,6 @@ package com.ail.revolut.app.service;
 import com.ail.revolut.app.NotEnoughFundsException;
 import com.ail.revolut.app.model.Account;
 
-// TODO develop AccountService, that will have business logic from Account class and this createAccount() method
 public interface AccountService {
 
 	Account createAccount();
