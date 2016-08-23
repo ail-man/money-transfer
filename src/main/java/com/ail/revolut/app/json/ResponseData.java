37a59e532f8b1229f6ecf3fb5e62c37631eac576
Artur Lomsadze <lomsadze@bpcbt.com>
2016-08-23 08:40:13 +0300

    Serializable json entities

--- src/main/java/com/ail/revolut/app/json/ResponseData.java
+++ src/main/java/com/ail/revolut/app/json/ResponseData.java
@@ -2,8 +2,12 @@ package com.ail.revolut.app.json;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class ResponseData {
+public class ResponseData implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private String value;
 	private String message;
 }
--- src/main/java/com/ail/revolut/app/json/TransferData.java
+++ src/main/java/com/ail/revolut/app/json/TransferData.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TransferData {
+public class TransferData implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private Long from;
 	private Long to;
 	private Long amount;
