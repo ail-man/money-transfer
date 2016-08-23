59046daa79615df4a0340a80b6b6579ce7141df8
Artur Lomsadze <lomsadze@bpcbt.com>
2016-08-23 12:40:17 +0300

    Fix rest consume xml

--- src/main/java/com/ail/revolut/app/json/ResponseData.java
+++ src/main/java/com/ail/revolut/app/json/ResponseData.java
@@ -2,9 +2,11 @@ package com.ail.revolut.app.json;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 @Data
+@XmlRootElement
 public class ResponseData implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/ail/revolut/app/json/TransferData.java
+++ src/main/java/com/ail/revolut/app/json/TransferData.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement
 public class TransferData implements Serializable {
 	private static final long serialVersionUID = 1L;
 
