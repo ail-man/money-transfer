de9a2e9f6547f724f05a4079d72305c75767b24a
Artur Lomsadze <lomsadze@bpcbt.com>
2016-12-22 13:03:23 +0300

    Added TODO

--- src/main/java/com/ail/revolut/app/db/HibernateContextHolder.java
+++ src/main/java/com/ail/revolut/app/db/HibernateContextHolder.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+// TODO use singleton
 public class HibernateContextHolder {
 	private static final String PERSISTENCE_UNIT_NAME = "h2unit";
 
